b831368f990131a31ced71cc5cc715aaaad7ae44
Source Academy User <No public email provided>
2023-10-11 21:45:32 +0800

    Changes made from Source Academy

---
+++ robotmission2.js
@@ -0,0 +1,35 @@
+// Qn 1
+
+// It is time to get started with the ultrasonic sensor
+// Write a program that will display (in the Source Academy) 
+// the distance as measured by the ultrasonic sensor once every second.
+// Note: you may need to apply a scaling factor.
+
+// Qn 2
+// Your next task is to use this distance information to 
+// prevent your robot from crashing into obstacles!
+// One such obstacle is the black box your robot came in!
+// Write a program to achieve the following behaviour:
+// Your robot should drive straight forward until it gets
+// close the Lego box. Once the robot is approximately 10 cm
+// from the Lego box it should stop and then reverse backwards for 30 cm.
+
+// Qn 3
+// Your next task is to program your robot to drive around obstacles.
+// For this task we will again using the Lego box as an obstacle.
+// Your program must randomly choose which direction to go around 
+// the box. That is, on approach, it should decide to either go around 
+// the left of the box or the right of the box (a 50-50 chance).
+// Write a program to navigate around the Lego box. Your robot may be 
+// placed pointing at any side of the box.
+
+const ultra
+
+function check_distance() {
+    return // stuff that pings sensor;
+}
+
+while (true) {
+    check_distance();
+    ev3_pause(1000);
+}
